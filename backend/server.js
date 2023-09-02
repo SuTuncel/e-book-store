59629ddecf5857923c5b2fc605d55f679e796950
ElifSuTuncel <83240777+SuTuncel@users.noreply.github.com>
2023-09-02 21:38:35 +0300

    refactored server.js and features added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoutes.js'
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,7 +9,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//middleware for parsing req body
 app.use(express.json());
+
+//middleware for handling cors policy
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
+
+
 
 app.get('/', (req, res)=> { 
     console.log(req);
@@ -16,29 +27,7 @@ app.get('/', (req, res)=> {
     return res.status(200).send('Welcome to book store');
 });
 
-// route for add a new book
-app.post('/books', async(req, res) => {
-    try {
-        if(!req.body.title || !req.body.author || !req.body.publishYear) {
-            return res.status(400).send({
-                message: 'Send all required fields: title, author, publishYear'
-            });
-        }
-
-        const newBook = {
-            title: req.body.title,
-            author: req.body.author,
-            publishYear: req.body.publishYear
-        }
-
-        const book = await Book.create(newBook);
-
-        return res.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({message: error.message});
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
 .connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology:true })
